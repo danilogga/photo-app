f0bb317b2bafe618025cfde1d670a2915e77119b
Danilo Carvalho <danilogga@gmail.com>
2019-04-06 17:30:34 -0400

    corrigindo região do AWS BUCKET

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -17,7 +17,7 @@ if Rails.env.production?
       provider: 'AWS',
       aws_access_key_id: ENV['S3_ACCESS_KEY'],
       aws_secret_access_key: ENV['S3_SECRET_KEY'],
-      region: 'Região Leste dos EUA (Ohio)'
+      region: 'us-east-2'
     }
     config.fog_directory = ENV['S3_BUCKET']
     config.fog_public = false
